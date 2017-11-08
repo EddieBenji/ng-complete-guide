5d7a5edba2ab8dca405398f0d9c71ca158587dae
Eduardo Canche <eduardo_benji@hotmail.com>
2017-11-08 17:15:17 -0600

    added recipe model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -3,12 +3,14 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css'],
+  styleUrls: [ './recipe-list.component.css' ],
   encapsulation: ViewEncapsulation.None
 })
 export class RecipeListComponent implements OnInit {
+  recipes = [];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
