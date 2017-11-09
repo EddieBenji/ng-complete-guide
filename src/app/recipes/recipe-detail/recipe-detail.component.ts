a3d270c0f15eb6694a0fb80006fbf52d4c5d8f4a
Eduardo Canche <eduardo_benji@hotmail.com>
2017-11-09 16:50:31 -0600

    list and detail components, are working together.

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -3,12 +3,13 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
-  styleUrls: ['./recipe-detail.component.css'],
+  styleUrls: [ './recipe-detail.component.css' ],
   encapsulation: ViewEncapsulation.None
 })
 export class RecipeDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
