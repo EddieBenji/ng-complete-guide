f44617b46455461c8798cac2d1638f428a91281d
Eduardo Canche <eduardo_benji@hotmail.com>
2017-11-24 16:22:02 -0600

    loaded feature according user selection.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,10 +8,15 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @Output() featureSelected = new EventEmitter<string>();
+
   constructor() {
   }
 
   ngOnInit() {
   }
 
+  onSelect(feature: string) {
+    this.featureSelected.emit(feature);
+  }
 }
