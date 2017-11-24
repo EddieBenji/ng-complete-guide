0b918318dcc4dc66719a9a48b2140949ce19df13
Eduardo Canche <eduardo_benji@hotmail.com>
2017-11-24 17:03:21 -0600

    'add ingredients' feature was added.

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,16 +1,31 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild, ViewEncapsulation } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: [ './shopping-edit.component.css' ],
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput') nameInput: ElementRef;
+  @ViewChild('amountInput') amountInput: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  onAddItem(event) {
+    event.preventDefault();
+    if (!this.nameInput.nativeElement.value.trim() || !this.amountInput.nativeElement.value.trim()) {
+      return;
+    }
+    this.ingredientAdded.emit(
+      new Ingredient(this.nameInput.nativeElement.value, this.amountInput.nativeElement.value)
+    );
   }
 
 }
