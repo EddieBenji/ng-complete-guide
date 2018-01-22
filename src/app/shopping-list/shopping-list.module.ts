c10e106be2e10ccb3433bd1206a0e4ddda81ea57
Eduardo Canche <eduardo_benji@hotmail.com>
2018-01-22 13:13:38 -0600

    added auth and shopping list modules.

---
+++ src/app/shopping-list/shopping-list.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
+import { ShoppingListComponent } from './shopping-list.component';
+import { FormsModule } from '@angular/forms';
+import { ShoppingListRoutingModule } from './shopping-list-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    ShoppingListRoutingModule
+  ],
+  declarations: [
+    ShoppingListComponent,
+    ShoppingEditComponent,
+  ]
+})
+export class ShoppingListModule {
+}
