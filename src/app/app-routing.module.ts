6013b67009155d1285cf3d71686cae7cb2a6b3ea
Eduardo Canche <eduardo_benji@hotmail.com>
2018-01-22 13:34:31 -0600

    added guard to remaining routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AuthGuard } from './auth/auth-guard.service';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   // Lazy loading:
-  { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule' }
+  { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule', canLoad: [AuthGuard] }
 ];
 
 @NgModule({
--- src/app/shopping-list/shopping-list-routing.module.ts
+++ src/app/shopping-list/shopping-list-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ShoppingListComponent } from './shopping-list.component';
+import { AuthGuard } from '../auth/auth-guard.service';
 
 const shoppingListRoutes: Routes = [
-  { path: 'shopping-list', component: ShoppingListComponent },
+  { path: 'shopping-list', component: ShoppingListComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
